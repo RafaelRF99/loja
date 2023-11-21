86e2e8c79893069ff7d02f6f90083c2578d23f90
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-21 20:00:48 -0300

    feat: icones edite/delete

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
   products: IProduct[] = [];
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
   constructor(private productService: ProductService) {}
 
